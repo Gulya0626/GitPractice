ae81171df567c739bd5c11017b869f57c7d6a1e8
Gulya0626 <gulyakg@yahoo.com>
2021-04-24 16:58:12 -0500

    Initial commit

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Adding Github Now");
+        System.out.println("Yay, stuff are on github");
+        System.out.println("Almost there");
+        System.out.println("testing");
+
+
+        //adding final wrap up code
+
+    }
+}
